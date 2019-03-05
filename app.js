dc184314c1021ead89c468874e93d7165f6111f0
Sbuda32 <sbuda32@gmail.com>
2019-03-05 18:53:05 +0200

    Modified app.js file

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ var express = require('express');
 
 const session = require('express-session');
 const bodyParser = require('body-parser');
+
+const route = require('./routes/');//game route
+
+const auth = require('./routes/auth');//authentication route
+
+const db = require('./models');
