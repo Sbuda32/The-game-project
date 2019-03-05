324aeec2a690e6a03ca4dd28f4230009bef32a46
Sbuda32 <sbuda32@gmail.com>
2019-03-05 19:06:53 +0200

    Added views folder and default.pug file

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ const route = require('./routes/');//game route
 const auth = require('./routes/auth');//authentication route
 
 var app = express();
+var serv = require('http').Server(app);
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+		secret:'work hard',
+		resave:true,
+		saveUninitialized: true,
+		cookie: { maxAge: 60000 }
+}));
 
 const db = require('./models');
 
 var flash = require('connect-flash');
+
+app.set('view engine','pug');
+
+app.use(express.static(__dirname +'/assets'));
