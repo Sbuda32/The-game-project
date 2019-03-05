644a955c29ca758406dbecde544782271bb5d114
Sbuda32 <sbuda32@gmail.com>
2019-03-05 18:50:27 +0200

    Added Schema definition in the accounts.js file

--- models/accounts.js
+++ models/accounts.js
@@ -1,2 +1,24 @@
 let mongoose = require('mongoose');
 let validator = require('validator');
+const bcrypt = require('bcrypt');
+
+let accountSchema = new mongoose.Schema({
+	username:{
+				type:String,
+				required:true,
+				unique:true
+
+			},
+
+	email: {
+				type:String,
+				required:true,
+				unique:true
+
+			},
+	password:{
+
+				type:String,
+				required:true
+			}
+});
